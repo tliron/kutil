tliron/rust-kutil

Combine crates into one

library/src/http/axum/host_router.rs
@@ -1,0 +1,88 @@
+use super::super::super::std::{collections::*, zerocopy::*};
+
+use {
+    ::axum::{extract::*, http::StatusCode, response::*, *},
+    axum_extra::extract::*,
+    tower::ServiceExt,
+};
+
+//
+// HostRouter
+//
+
+/// Host router.
+#[derive(Clone, Debug, Default)]
+pub struct HostRouter {
+    /// Routers by host (with optional port).
+    pub routers: FastHashMap<ByteString, Router>,
+
+    /// Fallback host (with optional port).
+    ///
+    /// If a router is not found for a host will fallback to the router for this host instead.
+    ///
+    /// There must be a router mapped to this host.
+    pub fallback_host: Option<ByteString>,
+}
+
+impl HostRouter {
+    /// Into [Router].
+    pub fn into_router(self) -> Option<Router> {
+        match self.routers.len() {
+            0 => None,
+            1 => self.routers.values().next().cloned(),
+            _ => Some(Router::default().fallback(host_routers_handler).with_state(self)),
+        }
+    }
+
+    /// Add.
+    pub fn add(&mut self, host_and_optional_port: ByteString, router: Router) {
+        self.routers.insert(host_and_optional_port, router);
+    }
+
+    /// Fallback [Router].
+    pub fn fallback_router(&mut self) -> Option<&mut Router> {
+        match &self.fallback_host {
+            Some(host_and_optional_port) => self.routers.get_mut(host_and_optional_port),
+            None => None,
+        }
+    }
+
+    /// Handle a [Request] by forwarding it to a router if possible.
+    pub async fn handle(&mut self, host_and_optional_port: ByteString, request: Request) -> Option<Response> {
+        let router = match self.routers.get_mut(&host_and_optional_port) {
+            Some(router) => {
+                tracing::debug!("router for host: {}", host_and_optional_port);
+                router
+            }
+
+            None => match self.fallback_router() {
+                Some(router) => {
+                    tracing::debug!("using fallback, no router for host: {}", host_and_optional_port);
+                    router
+                }
+
+                None => {
+                    tracing::debug!("no fallback and no router for host: {}", host_and_optional_port);
+                    return None;
+                }
+            },
+        };
+
+        Some(router.as_service().oneshot(request).await.expect("infallible"))
+    }
+}
+
+/// Axum request handler that calls [HostRouters::handle].
+///
+/// Expects the [HostRouters] to be available as state. See
+/// [Router::with_state](::axum::Router::with_state).
+pub async fn host_routers_handler(
+    State(mut host_routers): State<HostRouter>,
+    Host(host_and_optional_port): Host,
+    request: Request,
+) -> Response {
+    host_routers
+        .handle(host_and_optional_port.into(), request)
+        .await
+        .unwrap_or_else(|| StatusCode::NOT_FOUND.into_response())
+}

library/src/http/cache/body.rs
@@ -1,0 +1,162 @@
+use super::{
+    super::super::{
+        std::{collections::*, zerocopy::*},
+        transcoding::{transcode::*, *},
+    },
+    configuration::*,
+    weight::*,
+};
+
+use std::io;
+
+//
+// CachedBody
+//
+
+/// Cached HTTP response body.
+#[derive(Clone, Debug, Default)]
+pub struct CachedBody {
+    /// Representations.
+    pub representations: FastHashMap<Encoding, Bytes>,
+}
+
+impl CachedBody {
+    /// Constructor with an initial representation.
+    ///
+    /// If the `preferred_encoding` is different from the `encoding` then we will reencode.
+    ///
+    /// If an [Identity](Encoding::Identity) is created during this reencoding then it will also be
+    /// stored if `keep_identity_encoding` is true.
+    pub async fn new_with(
+        bytes: Bytes,
+        encoding: Encoding,
+        preferred_encoding: Encoding,
+        configuration: &EncodingConfiguration,
+    ) -> io::Result<Self> {
+        let mut representations = FastHashMap::default();
+
+        if preferred_encoding == encoding {
+            // It's already in the preferred encoding
+            representations.insert(preferred_encoding, bytes);
+        } else if encoding == Encoding::Identity {
+            tracing::debug!("encoding to {}", preferred_encoding);
+
+            let encoded_bytes = bytes.encode(&preferred_encoding).await?;
+
+            representations.insert(preferred_encoding, encoded_bytes);
+            if configuration.keep_identity_encoding {
+                representations.insert(Encoding::Identity, bytes);
+            }
+        } else if preferred_encoding == Encoding::Identity {
+            tracing::debug!("decoding from {}", encoding);
+
+            let identity_bytes = bytes.decode(&encoding).await?;
+
+            representations.insert(Encoding::Identity, identity_bytes);
+        } else {
+            tracing::debug!("reencoding from {} to {}", encoding, preferred_encoding);
+
+            let identity_bytes = bytes.decode(&encoding).await?;
+            let encoded_bytes = identity_bytes.encode(&preferred_encoding).await?;
+
+            representations.insert(preferred_encoding, encoded_bytes);
+            if configuration.keep_identity_encoding {
+                representations.insert(Encoding::Identity, identity_bytes);
+            }
+        }
+
+        Ok(Self { representations })
+    }
+
+    /// Returns the body [Bytes] in the specified encoding.
+    ///
+    /// If we don't have the specified encoding then we will reencode from another encoding,
+    /// storing the result so that we won't have to encode it again.
+    ///
+    /// If an [Identity](Encoding::Identity) is created during this reencoding then it will also be
+    /// stored if `keep_identity_encoding` is true.
+    ///
+    /// Returns a modified clone if reencoding caused a new encoding to be stored. Note that
+    /// cloning should be cheap due to our use of [Bytes].
+    pub async fn get(
+        &self,
+        encoding: &Encoding,
+        configuration: &EncodingConfiguration,
+    ) -> io::Result<(Bytes, Option<Self>)> {
+        match self.representations.get(encoding) {
+            Some(bytes) => Ok((bytes.clone(), None)),
+
+            None => match encoding {
+                Encoding::Identity => {
+                    // Decode
+                    for from_encoding in ENCODINGS_BY_DECODING_COST {
+                        if let Some(bytes) = self.representations.get(from_encoding) {
+                            tracing::debug!("decoding from {}", from_encoding);
+
+                            let identity_bytes = bytes.decode(from_encoding).await?;
+
+                            let mut modified = self.clone();
+                            modified.representations.insert(Encoding::Identity, identity_bytes.clone());
+
+                            return Ok((identity_bytes, Some(modified)));
+                        }
+                    }
+
+                    // This should never happen (but we don't want to panic here!)
+                    tracing::error!("no encodings");
+                    Ok((Default::default(), None))
+                }
+
+                to_encoding => {
+                    // Reencode
+                    if let Some(identity_bytes) = self.representations.get(&Encoding::Identity) {
+                        tracing::debug!("encoding to {}", to_encoding);
+
+                        let bytes = identity_bytes.encode(to_encoding).await?;
+
+                        let mut modified = self.clone();
+                        modified.representations.insert(to_encoding.clone(), bytes.clone());
+
+                        Ok((bytes, Some(modified)))
+                    } else {
+                        for from_encoding in ENCODINGS_BY_DECODING_COST {
+                            if let Some(bytes) = self.representations.get(from_encoding) {
+                                tracing::debug!("reencoding from {} to {}", from_encoding, to_encoding);
+
+                                let identity_bytes = bytes.decode(from_encoding).await?;
+                                let bytes = identity_bytes.encode(to_encoding).await?;
+
+                                let mut modified = self.clone();
+                                if configuration.keep_identity_encoding {
+                                    modified.representations.insert(Encoding::Identity, identity_bytes);
+                                }
+                                modified.representations.insert(to_encoding.clone(), bytes.clone());
+
+                                return Ok((bytes, Some(modified)));
+                            }
+                        }
+
+                        // This should never happen (but we don't want to panic here!)
+                        tracing::error!("no encodings");
+                        Ok((Default::default(), None))
+                    }
+                }
+            },
+        }
+    }
+}
+
+impl CacheWeight for CachedBody {
+    fn cache_weight(&self) -> usize {
+        const SELF_SIZE: usize = size_of::<CachedBody>();
+        const ENTRY_SIZE: usize = size_of::<Encoding>() + size_of::<Bytes>();
+
+        let mut size = SELF_SIZE;
+
+        for bytes in self.representations.values() {
+            size += ENTRY_SIZE + bytes.len();
+        }
+
+        size
+    }
+}

library/src/http/cache/key/common.rs
@@ -1,0 +1,186 @@
+use super::{
+    super::{
+        super::{super::std::zerocopy::*, headers::*, uri::*},
+        weight::*,
+    },
+    key::*,
+};
+
+use {
+    http::{header::*, uri::*, *},
+    std::{collections::*, fmt, hash::*},
+};
+
+//
+// CommonCacheKey
+//
+
+/// [CacheKey] implementation designed for common use cases.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct CommonCacheKey {
+    /// Method.
+    pub method: Method,
+
+    /// Optional path.
+    pub path: Option<ByteString>,
+
+    /// Optional query (sorted by key).
+    pub query: Option<QueryMap>,
+
+    /// Optional scheme.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub scheme: Option<Scheme>,
+
+    /// Optional host.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub host: Option<ByteString>,
+
+    /// Optional port.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub port: Option<u16>,
+
+    /// Optional media type.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub media_type: Option<MediaType>,
+
+    /// Optional languages (sorted).
+    ///
+    /// Not set by default but reserved for custom use.
+    pub languages: Option<BTreeSet<Language>>,
+
+    /// Optional extensions (sorted by key).
+    ///
+    /// Not set by default but reserved for custom use.
+    pub extensions: Option<BTreeMap<Bytes, Bytes>>,
+}
+
+impl CommonCacheKey {
+    /// Constructor.
+    pub fn new(
+        method: Method,
+        path: Option<ByteString>,
+        query: Option<QueryMap>,
+        scheme: Option<Scheme>,
+        host: Option<ByteString>,
+        port: Option<u16>,
+        media_type: Option<MediaType>,
+        languages: Option<BTreeSet<Language>>,
+        extensions: Option<BTreeMap<Bytes, Bytes>>,
+    ) -> Self {
+        Self { method, scheme, host, port, path, query, media_type, languages, extensions }
+    }
+}
+
+impl CacheKey for CommonCacheKey {
+    fn for_request(method: &Method, uri: &Uri, _headers: &HeaderMap) -> Self {
+        let (path, query) = match uri.path_and_query() {
+            Some(path_and_query) => (Some(path_and_query.path().into()), path_and_query.decoded_query_map()),
+            None => (None, None),
+        };
+
+        Self::new(method.clone(), path, query, None, None, None, None, None, None)
+    }
+}
+
+impl CacheWeight for CommonCacheKey {
+    fn cache_weight(&self) -> usize {
+        const SELF_SIZE: usize = size_of::<CommonCacheKey>();
+
+        let mut size = SELF_SIZE;
+
+        if let Some(host) = &self.host {
+            size += host.len();
+        }
+
+        if let Some(path) = &self.path {
+            size += path.len();
+        }
+
+        if let Some(query) = &self.query {
+            for (k, v) in query {
+                size += k.len() + v.len();
+            }
+        }
+
+        if let Some(media_type) = &self.media_type {
+            size += media_type.cache_weight();
+        }
+
+        if let Some(languages) = &self.languages {
+            for language in languages {
+                size += language.cache_weight();
+            }
+        }
+
+        if let Some(extensions) = &self.extensions {
+            for (k, v) in extensions {
+                size += k.len() + v.len();
+            }
+        }
+
+        size
+    }
+}
+
+impl fmt::Display for CommonCacheKey {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let scheme = self.scheme.as_ref().map(|scheme| scheme.as_str()).unwrap_or_default();
+        let host = self.host.as_ref().map(|host| AsRef::<str>::as_ref(host)).unwrap_or_default();
+        let port = self.port.map(|port| port.to_string()).unwrap_or_default();
+        let path = self.path.as_ref().map(|path| AsRef::<str>::as_ref(path)).unwrap_or_default();
+
+        let query = self
+            .query
+            .as_ref()
+            .map(|parameter| {
+                let mut string = String::default();
+                for (key, values) in parameter {
+                    for value in values {
+                        if !string.is_empty() {
+                            string += "&"
+                        }
+                        string += &format!("{}={}", key, value);
+                    }
+                }
+                string
+            })
+            .unwrap_or_default();
+
+        let media_type = self.media_type.as_ref().map(|media_type| media_type.to_string()).unwrap_or_default();
+
+        let languages = self
+            .languages
+            .as_ref()
+            .map(|languages| {
+                let languages: Vec<_> = languages.iter().map(|language| language.to_string()).collect();
+                languages.join(",")
+            })
+            .unwrap_or_default();
+
+        let extensions = self
+            .extensions
+            .as_ref()
+            .map(|extension| {
+                let mut string = String::default();
+                for (key, value) in extension {
+                    if !string.is_empty() {
+                        string += "&"
+                    }
+                    // We only display the length
+                    string += &format!("{}={}", key.len(), value.len());
+                }
+                string
+            })
+            .unwrap_or_default();
+
+        write!(
+            formatter,
+            "{}|{}|{}|{}|{}|{}|{}|{}|{}",
+            self.method, scheme, host, port, path, query, media_type, languages, extensions
+        )
+    }
+}

tliron/kutil

library/src/http/axum/host_router.rs
@@ -1,0 +1,88 @@
+use super::super::super::std::{collections::*, immutable::*};
+
+use {
+    ::axum::{extract::*, http::StatusCode, response::*, *},
+    axum_extra::extract::*,
+    tower::ServiceExt,
+};
+
+//
+// HostRouter
+//
+
+/// Host router.
+#[derive(Clone, Debug, Default)]
+pub struct HostRouter {
+    /// Routers by host (with optional port).
+    pub routers: FastHashMap<ByteString, Router>,
+
+    /// Fallback host (with optional port).
+    ///
+    /// If a router is not found for a host will fallback to the router for this host instead.
+    ///
+    /// There must be a router mapped to this host.
+    pub fallback_host: Option<ByteString>,
+}
+
+impl HostRouter {
+    /// Into [Router].
+    pub fn into_router(self) -> Option<Router> {
+        match self.routers.len() {
+            0 => None,
+            1 => self.routers.values().next().cloned(),
+            _ => Some(Router::default().fallback(host_routers_handler).with_state(self)),
+        }
+    }
+
+    /// Add.
+    pub fn add(&mut self, host_and_optional_port: ByteString, router: Router) {
+        self.routers.insert(host_and_optional_port, router);
+    }
+
+    /// Fallback [Router].
+    pub fn fallback_router(&mut self) -> Option<&mut Router> {
+        match &self.fallback_host {
+            Some(host_and_optional_port) => self.routers.get_mut(host_and_optional_port),
+            None => None,
+        }
+    }
+
+    /// Handle a [Request] by forwarding it to a router if possible.
+    pub async fn handle(&mut self, host_and_optional_port: ByteString, request: Request) -> Option<Response> {
+        let router = match self.routers.get_mut(&host_and_optional_port) {
+            Some(router) => {
+                tracing::debug!("router for host: {}", host_and_optional_port);
+                router
+            }
+
+            None => match self.fallback_router() {
+                Some(router) => {
+                    tracing::debug!("using fallback, no router for host: {}", host_and_optional_port);
+                    router
+                }
+
+                None => {
+                    tracing::debug!("no fallback and no router for host: {}", host_and_optional_port);
+                    return None;
+                }
+            },
+        };
+
+        Some(router.as_service().oneshot(request).await.expect("infallible"))
+    }
+}
+
+/// Axum request handler that calls [HostRouters::handle].
+///
+/// Expects the [HostRouters] to be available as state. See
+/// [Router::with_state](::axum::Router::with_state).
+pub async fn host_routers_handler(
+    State(mut host_routers): State<HostRouter>,
+    Host(host_and_optional_port): Host,
+    request: Request,
+) -> Response {
+    host_routers
+        .handle(host_and_optional_port.into(), request)
+        .await
+        .unwrap_or_else(|| StatusCode::NOT_FOUND.into_response())
+}

library/src/http/cache/body.rs
@@ -1,0 +1,162 @@
+use super::{
+    super::super::{
+        std::{collections::*, immutable::*},
+        transcoding::{transcode::*, *},
+    },
+    configuration::*,
+    weight::*,
+};
+
+use std::io;
+
+//
+// CachedBody
+//
+
+/// Cached HTTP response body.
+#[derive(Clone, Debug, Default)]
+pub struct CachedBody {
+    /// Representations.
+    pub representations: FastHashMap<Encoding, Bytes>,
+}
+
+impl CachedBody {
+    /// Constructor with an initial representation.
+    ///
+    /// If the `preferred_encoding` is different from the `encoding` then we will reencode.
+    ///
+    /// If an [Identity](Encoding::Identity) is created during this reencoding then it will also be
+    /// stored if `keep_identity_encoding` is true.
+    pub async fn new_with(
+        bytes: Bytes,
+        encoding: Encoding,
+        preferred_encoding: Encoding,
+        configuration: &EncodingConfiguration,
+    ) -> io::Result<Self> {
+        let mut representations = FastHashMap::default();
+
+        if preferred_encoding == encoding {
+            // It's already in the preferred encoding
+            representations.insert(preferred_encoding, bytes);
+        } else if encoding == Encoding::Identity {
+            tracing::debug!("encoding to {}", preferred_encoding);
+
+            let encoded_bytes = bytes.encode(&preferred_encoding).await?;
+
+            representations.insert(preferred_encoding, encoded_bytes);
+            if configuration.keep_identity_encoding {
+                representations.insert(Encoding::Identity, bytes);
+            }
+        } else if preferred_encoding == Encoding::Identity {
+            tracing::debug!("decoding from {}", encoding);
+
+            let identity_bytes = bytes.decode(&encoding).await?;
+
+            representations.insert(Encoding::Identity, identity_bytes);
+        } else {
+            tracing::debug!("reencoding from {} to {}", encoding, preferred_encoding);
+
+            let identity_bytes = bytes.decode(&encoding).await?;
+            let encoded_bytes = identity_bytes.encode(&preferred_encoding).await?;
+
+            representations.insert(preferred_encoding, encoded_bytes);
+            if configuration.keep_identity_encoding {
+                representations.insert(Encoding::Identity, identity_bytes);
+            }
+        }
+
+        Ok(Self { representations })
+    }
+
+    /// Returns the body [Bytes] in the specified encoding.
+    ///
+    /// If we don't have the specified encoding then we will reencode from another encoding,
+    /// storing the result so that we won't have to encode it again.
+    ///
+    /// If an [Identity](Encoding::Identity) is created during this reencoding then it will also be
+    /// stored if `keep_identity_encoding` is true.
+    ///
+    /// Returns a modified clone if reencoding caused a new encoding to be stored. Note that
+    /// cloning should be cheap due to our use of [Bytes].
+    pub async fn get(
+        &self,
+        encoding: &Encoding,
+        configuration: &EncodingConfiguration,
+    ) -> io::Result<(Bytes, Option<Self>)> {
+        match self.representations.get(encoding) {
+            Some(bytes) => Ok((bytes.clone(), None)),
+
+            None => match encoding {
+                Encoding::Identity => {
+                    // Decode
+                    for from_encoding in ENCODINGS_BY_DECODING_COST {
+                        if let Some(bytes) = self.representations.get(from_encoding) {
+                            tracing::debug!("decoding from {}", from_encoding);
+
+                            let identity_bytes = bytes.decode(from_encoding).await?;
+
+                            let mut modified = self.clone();
+                            modified.representations.insert(Encoding::Identity, identity_bytes.clone());
+
+                            return Ok((identity_bytes, Some(modified)));
+                        }
+                    }
+
+                    // This should never happen (but we don't want to panic here!)
+                    tracing::error!("no encodings");
+                    Ok((Default::default(), None))
+                }
+
+                to_encoding => {
+                    // Reencode
+                    if let Some(identity_bytes) = self.representations.get(&Encoding::Identity) {
+                        tracing::debug!("encoding to {}", to_encoding);
+
+                        let bytes = identity_bytes.encode(to_encoding).await?;
+
+                        let mut modified = self.clone();
+                        modified.representations.insert(to_encoding.clone(), bytes.clone());
+
+                        Ok((bytes, Some(modified)))
+                    } else {
+                        for from_encoding in ENCODINGS_BY_DECODING_COST {
+                            if let Some(bytes) = self.representations.get(from_encoding) {
+                                tracing::debug!("reencoding from {} to {}", from_encoding, to_encoding);
+
+                                let identity_bytes = bytes.decode(from_encoding).await?;
+                                let bytes = identity_bytes.encode(to_encoding).await?;
+
+                                let mut modified = self.clone();
+                                if configuration.keep_identity_encoding {
+                                    modified.representations.insert(Encoding::Identity, identity_bytes);
+                                }
+                                modified.representations.insert(to_encoding.clone(), bytes.clone());
+
+                                return Ok((bytes, Some(modified)));
+                            }
+                        }
+
+                        // This should never happen (but we don't want to panic here!)
+                        tracing::error!("no encodings");
+                        Ok((Default::default(), None))
+                    }
+                }
+            },
+        }
+    }
+}
+
+impl CacheWeight for CachedBody {
+    fn cache_weight(&self) -> usize {
+        const SELF_SIZE: usize = size_of::<CachedBody>();
+        const ENTRY_SIZE: usize = size_of::<Encoding>() + size_of::<Bytes>();
+
+        let mut size = SELF_SIZE;
+
+        for bytes in self.representations.values() {
+            size += ENTRY_SIZE + bytes.len();
+        }
+
+        size
+    }
+}

library/src/http/cache/key/common.rs
@@ -1,0 +1,186 @@
+use super::{
+    super::{
+        super::{super::std::immutable::*, headers::*, uri::*},
+        weight::*,
+    },
+    key::*,
+};
+
+use {
+    http::{header::*, uri::*, *},
+    std::{collections::*, fmt, hash::*},
+};
+
+//
+// CommonCacheKey
+//
+
+/// [CacheKey] implementation designed for common use cases.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct CommonCacheKey {
+    /// Method.
+    pub method: Method,
+
+    /// Optional path.
+    pub path: Option<ByteString>,
+
+    /// Optional query (sorted by key).
+    pub query: Option<QueryMap>,
+
+    /// Optional scheme.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub scheme: Option<Scheme>,
+
+    /// Optional host.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub host: Option<ByteString>,
+
+    /// Optional port.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub port: Option<u16>,
+
+    /// Optional media type.
+    ///
+    /// Not set by default but reserved for custom use.
+    pub media_type: Option<MediaType>,
+
+    /// Optional languages (sorted).
+    ///
+    /// Not set by default but reserved for custom use.
+    pub languages: Option<BTreeSet<Language>>,
+
+    /// Optional extensions (sorted by key).
+    ///
+    /// Not set by default but reserved for custom use.
+    pub extensions: Option<BTreeMap<Bytes, Bytes>>,
+}
+
+impl CommonCacheKey {
+    /// Constructor.
+    pub fn new(
+        method: Method,
+        path: Option<ByteString>,
+        query: Option<QueryMap>,
+        scheme: Option<Scheme>,
+        host: Option<ByteString>,
+        port: Option<u16>,
+        media_type: Option<MediaType>,
+        languages: Option<BTreeSet<Language>>,
+        extensions: Option<BTreeMap<Bytes, Bytes>>,
+    ) -> Self {
+        Self { method, scheme, host, port, path, query, media_type, languages, extensions }
+    }
+}
+
+impl CacheKey for CommonCacheKey {
+    fn for_request(method: &Method, uri: &Uri, _headers: &HeaderMap) -> Self {
+        let (path, query) = match uri.path_and_query() {
+            Some(path_and_query) => (Some(path_and_query.path().into()), path_and_query.decoded_query_map()),
+            None => (None, None),
+        };
+
+        Self::new(method.clone(), path, query, None, None, None, None, None, None)
+    }
+}
+
+impl CacheWeight for CommonCacheKey {
+    fn cache_weight(&self) -> usize {
+        const SELF_SIZE: usize = size_of::<CommonCacheKey>();
+
+        let mut size = SELF_SIZE;
+
+        if let Some(host) = &self.host {
+            size += host.len();
+        }
+
+        if let Some(path) = &self.path {
+            size += path.len();
+        }
+
+        if let Some(query) = &self.query {
+            for (k, v) in query {
+                size += k.len() + v.len();
+            }
+        }
+
+        if let Some(media_type) = &self.media_type {
+            size += media_type.cache_weight();
+        }
+
+        if let Some(languages) = &self.languages {
+            for language in languages {
+                size += language.cache_weight();
+            }
+        }
+
+        if let Some(extensions) = &self.extensions {
+            for (k, v) in extensions {
+                size += k.len() + v.len();
+            }
+        }
+
+        size
+    }
+}
+
+impl fmt::Display for CommonCacheKey {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let scheme = self.scheme.as_ref().map(|scheme| scheme.as_str()).unwrap_or_default();
+        let host = self.host.as_ref().map(|host| AsRef::<str>::as_ref(host)).unwrap_or_default();
+        let port = self.port.map(|port| port.to_string()).unwrap_or_default();
+        let path = self.path.as_ref().map(|path| AsRef::<str>::as_ref(path)).unwrap_or_default();
+
+        let query = self
+            .query
+            .as_ref()
+            .map(|parameter| {
+                let mut string = String::default();
+                for (key, values) in parameter {
+                    for value in values {
+                        if !string.is_empty() {
+                            string += "&"
+                        }
+                        string += &format!("{}={}", key, value);
+                    }
+                }
+                string
+            })
+            .unwrap_or_default();
+
+        let media_type = self.media_type.as_ref().map(|media_type| media_type.to_string()).unwrap_or_default();
+
+        let languages = self
+            .languages
+            .as_ref()
+            .map(|languages| {
+                let languages: Vec<_> = languages.iter().map(|language| language.to_string()).collect();
+                languages.join(",")
+            })
+            .unwrap_or_default();
+
+        let extensions = self
+            .extensions
+            .as_ref()
+            .map(|extension| {
+                let mut string = String::default();
+                for (key, value) in extension {
+                    if !string.is_empty() {
+                        string += "&"
+                    }
+                    // We only display the length
+                    string += &format!("{}={}", key.len(), value.len());
+                }
+                string
+            })
+            .unwrap_or_default();
+
+        write!(
+            formatter,
+            "{}|{}|{}|{}|{}|{}|{}|{}|{}",
+            self.method, scheme, host, port, path, query, media_type, languages, extensions
+        )
+    }
+}